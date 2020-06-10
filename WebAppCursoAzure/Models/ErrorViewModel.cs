776a6f5de38e93ad24f2f12a4890d78379f3106e
Santiago Cepeda <santyagor@outlook.com>
2020-06-10 12:04:25 -0500

    Proyecto integrado

---
+++ WebAppCursoAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppCursoAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
